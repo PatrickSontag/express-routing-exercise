4985709254e39ec7c5a1f4aea47bf2cd4d24a6f7
Patrick Sontag <pmsontag84@gmail.com>
2023-01-04 06:58:22 -0600

    median collect inputs

--- app.js
+++ app.js
@@ -28,6 +28,21 @@ app.get('/mean', (req, res) => {
     return res.send(`MEAN = ${mean}`);
 });
 
+app.get('/median', (req, res) => {
+    let length;
+    let nums;
+    if (req.query.nums) {
+        // comma separated "nums" to array of "nums" 
+        nums = req.query.nums.split(',');
+        // string nums to number nums
+        nums = nums.map(n => n * 1);
+        nums = nums.sort();
+        
+        length = req.query.nums.length;
+        }
+    return res.send(`MEDIAN = `);
+});
+
 app.listen(3000, function() {
 console.log('Server started on port 3000.');
 });
